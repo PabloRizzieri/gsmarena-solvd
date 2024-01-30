77aa9367a018de8bc61dd3e977123d58b3f32055
Pablo Rizzieri <ppablorizzieri@gmail.com>
2024-01-29 23:13:40 -0300

    feat: Create Main class and configure ChromeDriver
    test: Add ExampleTest to MainTest
    
    Introduce the ExampleTest method within MainTest to demonstrate a basic test scenario. This test navigates to the 'https://www.gsmarena.com' website.
    
    Changes:
    - Added ExampleTest method in MainTest

--- src/test/java/MainTest.java
+++ src/test/java/MainTest.java
@@ -2,6 +2,7 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
 
 
 public class MainTest {
@@ -14,6 +15,11 @@ public class MainTest {
 
         // Initialize ChromeDriver
         driver = new ChromeDriver();
+    }
+
+    @Test
+    public void ExampleTest(){
+        driver.get("https://www.gsmarena.com");
     }
 
     @AfterMethod
