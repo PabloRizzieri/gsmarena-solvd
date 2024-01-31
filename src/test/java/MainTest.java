454a0fa3ed6af4b048584e266b2bcdafcc2de069
Pablo Rizzieri <ppablorizzieri@gmail.com>
2024-01-31 13:20:56 -0300

    [SLIP-5] Refactor Selenium WebDriver setup and test in MainTest class
    This commit updates the Selenium WebDriver setup and test in the MainTest class. The changes include:
    
    - Switching to WebDriverManager for managing the ChromeDriver setup.
    - Modifying the setup and teardown methods to use annotations from TestNG.
    - Refactoring the test method to navigate to a different URL and assert the page title.
    
    These changes improve code organization, maintainability, and leverage WebDriverManager for managing WebDriver binaries.

--- src/test/java/MainTest.java
+++ src/test/java/MainTest.java
@@ -1,30 +1,35 @@
+import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
-import org.testng.annotations.AfterMethod;
-import org.testng.annotations.BeforeMethod;
-import org.testng.annotations.Test;
+import org.testng.Assert;
+import org.testng.annotations.*;
 
 
 public class MainTest {
-    private WebDriver driver;
+
+    WebDriver driver;
 
     @BeforeMethod
-    public void setUp() {
-        // Specifying the location of the Chrome Driver
-        System.setProperty("webdriver.chrome.driver", "src/test/drivers/chromedriver.exe");
+    static void setupClass(){
+        WebDriverManager.chromedriver().setup();
+    }
 
-        // Initialize ChromeDriver
+    @BeforeTest
+    void setupTest(){
         driver = new ChromeDriver();
     }
 
-    @Test
-    public void ExampleTest(){
-        driver.get("https://www.gsmarena.com");
-    }
-
-    @AfterMethod
-    public void tearDown() {
-        // Close the driver after each test
+    @AfterTest
+    void teardown(){
         driver.quit();
     }
+
+    @Test
+    void test(){
+        driver.get("https://bonigarcia.dev/selenium-webdriver-java/");
+        String tittle = driver.getTitle();
+
+        Assert.assertTrue(tittle.contains("Selenium WebDriver"));
+    }
+
 }
