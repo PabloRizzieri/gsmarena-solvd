d2f527013b28b927b3041206f8eb0a940844a5fc
lucasp149 <lucasp149@gmail.com>
2024-02-02 11:16:29 -0300

    dev commit 003:
    - LoginFormTests now extends BaseTest
    - Created Properties attribute
    - added the block to load properties
    - created the setUpTest method

--- src/test/java/com/solvd/theinternet_solvd/loginFormTest/LoginFormTests.java
+++ src/test/java/com/solvd/theinternet_solvd/loginFormTest/LoginFormTests.java
@@ -1,10 +1,10 @@
 package com.solvd.theinternet_solvd.loginFormTest;
 
-import com.google.gson.internal.bind.util.ISO8601Utils;
 import com.solvd.theinternet_solvd.BaseTest;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
 
 import java.io.FileInputStream;
-import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.io.InputStream;
 import java.util.Properties;
@@ -13,13 +13,20 @@ public class LoginFormTests extends BaseTest {
 
     Properties prop = new Properties();
 
-
+    /*
+     * This block loads the propesties from config.properties file
+     * */
     {
         try (InputStream inputStream = new FileInputStream("src/test/resources/config.properties")) {
             prop.load(inputStream);
         } catch (IOException e) {
             throw new RuntimeException(e);
         }
+    }
+
+    @BeforeMethod
+    public void setUpTest(){
+        driver.get("https://the-internet.herokuapp.com/login");
     }
 
 
