8e974612d72805bc235bbe210444765badac147c
lucasp149 <lucasp149@gmail.com>
2024-02-02 11:08:19 -0300

    dev commit 003:
    - LoginFormTests now extends BaseTest
    - Created Properties attribute
    - added the block to load properties

--- src/test/java/com/solvd/theinternet_solvd/loginFormTest/LoginFormTests.java
+++ src/test/java/com/solvd/theinternet_solvd/loginFormTest/LoginFormTests.java
@@ -1,4 +1,27 @@
 package com.solvd.theinternet_solvd.loginFormTest;
 
-public class LoginFormTests {
+import com.google.gson.internal.bind.util.ISO8601Utils;
+import com.solvd.theinternet_solvd.BaseTest;
+
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Properties;
+
+public class LoginFormTests extends BaseTest {
+
+    Properties prop = new Properties();
+
+
+    {
+        try (InputStream inputStream = new FileInputStream("src/test/resources/config.properties")) {
+            prop.load(inputStream);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+
+
 }
