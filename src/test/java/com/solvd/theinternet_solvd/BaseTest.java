8b7c90715308162ea2d039f6703f62c7c657c6a6
Pablo Rizzieri <ppablorizzieri@gmail.com>
2024-02-02 18:53:38 -0300

    [SLIP-19] refactor: Simplify MainTest to BaseTest
    Refactor the abstract class `MainTest` into a simpler and more concise class named `BaseTest`. The new class retains the essential setup and teardown methods for the WebDriver but omits unnecessary details. This change improves code readability and maintains a clear separation of concerns.
    
    Changes:
    - Renamed `MainTest` to `BaseTest`.
    - Removed unnecessary `setupTest` method call in the `setupClass` method.
    - Made adjustments for improved code simplicity and maintainability.

--- src/test/java/com/solvd/theinternet_solvd/BaseTest.java
+++ src/test/java/com/solvd/theinternet_solvd/BaseTest.java
@@ -2,26 +2,19 @@ package com.solvd.theinternet_solvd;
 
 import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.chrome.ChromeDriver;
-import org.testng.Assert;
 import org.testng.annotations.*;
 
 
-public abstract class MainTest {
+public class BaseTest {
 
     protected WebDriver driver;
 
     @BeforeClass
     public void setupClass(){
-        WebDriverManager.chromedriver().setup();
-        driver = new ChromeDriver();
-        setupTest();
+        driver = WebDriverManager.chromedriver().create();
     }
 
-    @BeforeTest
-    abstract void setupTest();
-
-    @AfterTest
+    @AfterClass
     public void tearDown(){
         driver.quit();
     }
